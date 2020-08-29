746f108f553b373a4dc188faf5a4bc0b6a120438
Othniel Konan <othniel@elenjicalsolutions.com>
2020-08-29 22:47:00 +0200

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './card.styles.css'
+
+export const Card = (props) => (
+  <div className='card-container'>
+    <img  alt='monster' 
+          src={`https://robohash.org/${props.monster.name}?size=180x180`}/>
+    <h3> {props.monster.name} </h3>
+    <p> {props.monster.email} </p>
+  </div>
+)
